43d180ebbb7fcbca5615358505cf5fdec2dc4a1a
pranayajajoo <pranayajajoo@gmail.com>
2024-12-11 11:01:43 -0700

    layput improvements and research

--- page.js
+++ page.js
@@ -1,198 +1,25 @@
 var data = {
     "research": [
         {
-            img: 'images_pranaya/Wayfair.jpg',
-            title: 'Wayfair LLC',
-            teams: [
-                {
-                    teamName: 'Senior Data Analyst | Planning and Inventory Management',
-                    period: 'Jan 2023 - Present',
-                    text1:'My role in Planning and Inventory Management was to support my stakeholders, inventory managers, order and position inventory globally. To that effect, I - ',
-                    text2: [
-                            ['Developed an internal tool that generated more accurate forecast and produced order quantities. This tool has since been tested for a year, used to over $800M of inventory, and productionalized into a web application.'],
-                            ['Incorporated fine-tuned large language models to enable text-to-SQL querying and retrieval augmented generation (RAG) for extracting factual information from document databases.'],
-                            ['Defined new KPIs and developed several data pipelines and data visualization reports to enable better operations for my stakeholders.']
-                        ]
-                },
-                {
-                    teamName: 'Senior Data Analyst | Returns Monetization Channel Growth',
-                    period: 'Aug 2022 - Jan 2023',
-                    // text1: 'My role in Returns monetization Channel Growth was to provide tech support to the development of new monetization channels for returned inventory. To that effect, I - ',
-                    text2: [['Used regression models determine the most profitable products categories to sell in Wayfair’s outlet store and to determine storage plans for large-parcel and small-parcel inventory.'],
-                            ['Developed an internal tool that would extract details like currently listed price and item description for a product and used API connectors to send this information to a third-party point of sale system.']
-                          ]
-                },
-                {
-                    teamName: 'Senior Data Analyst | Partner Operations for Specialty Retail Brands',
-                    period: 'Mar 2022 - aug 2022',
-                    text1: '',
-                    text2: [
-                            ['Created new product and supplier databases for Specialty Retail Brands, Flagship Brands, and Physical Retail.'],
-                            ['Developed and maintained long-term reporting for All Modern, Joss and Main, Birch Lane, and Wayfair\'s physical retail business in the form of KPI reports and dashboards; Created a query database by writing custom SQL queries based on business requirements.']
-                          ]
-                }
-            ]
+                img: 'images_pranaya/rlzero.jpg',
+                title: 'RLZero: Zero-Shot Language to Behaviors without any Supervision',
+                teams: [
+                    {
+                        teamName: 'Harshit Sikchi*, Siddhant Agarwal*, Pranaya Jajoo*, Samyak Parajuli*, Max Rudolph*, Peter Stone, Amy Zhang, Scott Niekum',
+                        period: 'arxiv.org/abs/2412.05718',
+                        text1: 'https://hari-sikchi.github.io/rlzero/',
+                        text2: 'Releasing soon!',
+                    }
+                   ]
         },
-        {
-            img: '/images_pranaya/Wayfair.jpg',
-            title: 'Avant LLC',
-            teams: [
-                {
-                    teamName: 'Senior Data Analyst | Product and Data',
-                    period: 'Jan 2022 - Feb 2022',
-                    text1: 'Saved labor costs in customer service by -',
-                    text2: [
-                            ['Using planning algorithms like Dijkstra\'s and A-star to identify paths that were most used and amenable to be converted to a self service pipeline and building them. Some pipelines that I added are - \‘Text-to-Pay\’ and \‘Schedule Callbacks\’.']
-                          ]
-                },
-                {
-                    teamName: 'Data Analyst | Operations Strategy',
-                    period: 'Dec 2020 - Dec 2021',
-                    text1: '',
-                    text2: [
-                            ['Automate agent scheduling for customer interactions: Proposed and developed to use online learning strategies like Upper-Confidence Bound (UCB) to automate agent-customer interaction and provably reduce customer regret and facilitate even load distribution.'],
-                            ['Livevox SMS and dialing campaigns implementation: Deployed LiveVox, a collections communications platform, to replace a disparate infrastructure of in-house and third party applications that managed outbound dialing, email, and SMS leading to estimated savings of $100K   a year. Scripted python jobs for auto-migrating customer communication data from Livevox to Avant\’s CRM; deployed monitoring and testing for customer communication campaigns.']
-                          ]
-                }
-            ]
-        },
-        {
-            img: '../images_pranaya/Wayfair.jpg',
-            title: 'Runaway',
-            teams: [
-                {
-                    teamName: 'Technical Product Manager | Web and Mobile Application',
-                    period: 'Jun 2020 - Dec 2020',
-                    text1: '',
-                    text2: [
-                            ['Created the product road-map and led the development and launch of a front-end mobile application and a back-end web application in the mental health space.']
-                          ]
-                }
-            ]
-        },
-        {
-            img: '../images_pranaya/Wayfair.jpg',
-            title: 'Illinois Business Consulting',
-            teams: [
-                {
-                    teamName: 'Technical Consultant for Aldi | Regression, Forecasting models',
-                    period: 'Jan 2020 - May 2020',
-                    text1: '',
-                    text2: [
-                            ['Improved logistical services to reduce labour costs by standardizing pellet delivery throughout the week using regression models to find the best delivery days for stores by region.'],
-                            ['Created a database-to-google sheets tool that will automate the process of identifying the most efficient delivery operation based on dynamically changing data from stores located globally now and in the future.']
-                          ]
-                }
-            ]
-        },
-        {
-            img: '../images_pranaya/Wayfair.jpg',
-            title: 'Phi Optics',
-            teams: [
-                {
-                    teamName: 'Technical Manager (part-time) | Computer Vision, Startup',
-                    period: 'Jan 2020 - May 2020',
-                    text1: '',
-                    text2: [
-                            ['Led a team of 4 in developing computer vision techniques for live cell imaging. We focused on curating datasets and building upon methods like YOLO for detection and Mask-RCNN for segmentation.']
-                          ]
-                }
-            ]
-        },
-        {
-            img: '../images_pranaya/Wayfair.jpg',
-            title: 'Bhabha Atomic Research Center',
-            teams: [
-                {
-                    teamName: 'Research Intern | Multimodal Machine Learning',
-                    period: 'Jun 2018 - Sep 2018',
-                    text1: '',
-                    text2: [
-                            ['Worked under lead Scientific Officer, Paraj Walinjkar, to predict the impact of radiation on surroundings depending on environmental factors. The data sources varied from readings from specialized instruments to weather readings. The project resulted in eliminating the need for analog and manual analysis and reducing costs by $6000.']
-                          ]
-                }
-            ]
-        },
-        {
-            img: '../images_pranaya/Wayfair.jpg',
-            title: 'RR Kabel Limited',
-            teams: [
-                {
-                    teamName: 'Analyst Intern | Time Series Modelling',
-                    period: 'Mar 2018 - May 2018',
-                    text1: '',
-                    text2: [
-                            ['Designed a time-series model to keep track of key Quality Assurance parameters in wire and cable manufacturing as a function of past and present demand, historical raw material quality characterizations, and human workforce.']
-                          ]
-                }
-            ]
-        },
-        {
-            img: '../images_pranaya/Wayfair.jpg',
-            title: 'Siemens Limited',
-            teams: [
-                {
-                    teamName: 'SUpply Chain Intern | Data Visualization',
-                    period: 'Jun 2017 - Aug 2017',
-                    text1: '',
-                    text2: [
-                            ['Created a Tableau dashboard that tracked the required cycle time vs current production rate for circuit breakers, employee distribution on the assembly line, and status of defective appliances, raw materials, and missing orders, increasing the production rate by 3%.']
-                          ]
-                }
-            ]
-        },
-        
-
     ],
     "projects": [
-        {
-            img: 'images/',
-            title: 'GSCMI Conference held at Purdue University',
-            text: [
-                    ['Selected to represent UIUC at the Global Supply Chain Management Initiative (GSCMI) Case Competition to develop a strategy to transform a company\'s global supply chain to accommodate changing production and transportation costs and exchange rates while maintaining steady sales.']
-                  ]
-        },
-        {
-            img: 'images/',
-            title: 'British Petroleum Professional Responsibility Strategy Competition 2019 | First Runner Up',
-            text: [
-                    ['Scraped data from web on energy usage and carbon dioxide emissions from various companies. Learned an inverse model attributing importance to features using Shapley values.'],
-                    ['Presented a data-backed insight and strategy on transforming the business to optimize for these quantities within practical constraints.']
-                  ]
-        },
-        {
-            img: 'images/',
-            title: 'Automatic Traffic Control using Computer Vision | Bachelor Thesis',
-            text: [
-                    ['Worked on a project to address the vehicular traffic problems in Mumbai by devising an effective system using data collected from 11 traffic signal cameras and applying object detection and tracking techniques to identify traffic patterns.']
-                  ]
-        }],
+        ],
     "volunteering": [
-        {
-            img: 'images/',
-            title: 'Rubik\'s Cube Mumbai Open (RCMO), RCMO Head 2017-2018',
-            text: 'Led a team of 50+ students to conduct an international cubing competition and to organize two workshops with over 60 participants; oversaw \textit{two national records in the Limca Book of Records} with my team.'
-        }],
+        ],
     "teaching": [
-        {
-            img: 'images/',
-            title: 'University of Illinois - Urbana Champaign',
-            text: 'Assisted in teaching, mentoring, assessment, and grading activities for undergraduate courses BADM 312 (Designing and Managing Organizations), BADM 329 (New Product Development), and BADM 365 (New Product Marketing) and graduate courses BADM 508 (Leadership and Teams), MBA 551 (Strategic Innovation), BIOE 573 (Managing Business Operations), BADM 525 (New Product Development).'
-        }],
-    "education": [
-        {
-            img: 'images/',
-            university: 'University of Illinois - Urbana Champaign',
-            degree: 'Master of Science in Technology Management | Recepient of the Award for Academic Excellence',
-            text: ''
-        },
-        {
-            img: 'images/',
-            university: 'Veermata Jijabai Technological Institute',
-            degree: 'Bachelor of Technology in Electrical Engineering',
-            text: ''
-        }
-    
+        ],
+    "education": [    
     ],
     "news": [
     ],
@@ -270,11 +97,15 @@ function getProjectHTML(proj) {
 
 function getResearchHTML(proj) {
     var html = '<tr>';
-    // Add image
-    // html += '<td class="image-td"><img class="project-img" src="' + proj.img + '"></td>';
-    // html += '<iframe width="420" height="315" src="' + proj.img + '> </iframe > '
-    // Add title and description
-    html += '<td class="description-td"><h3>' + proj.title + '</h3>'
+    // Adding the image
+    if ('img' in proj && proj.img.trim() !== '') {
+        html += '<td class="image-td"><img class="project-img" src="' + proj.img + '" alt="Project Image"></td>';
+    } else {
+        html += '<td class="image-td">No image available</td>'; // Fallback when there is no image
+    }
+
+    // Add title and team details
+    html += '<td class="description-td"><h3>' + proj.title + '</h3>';
 
     if ('teams' in proj && Array.isArray(proj.teams)) {
         for (var i = 0; i < proj.teams.length; i++) {
@@ -287,19 +118,17 @@ function getResearchHTML(proj) {
             if ('period' in team) {
                 // Remove the word "team" from the team title
                 var teamTitle = team.period.replace('team', '').trim();
-                html += '<p><i>' + teamTitle + ':</i></p>';
+                html += '<p><a href="' + teamTitle + '" target="_blank"><i>Paper</i></a></p>';
             }
             if ('text1' in team) {
                 // Remove the word "team" from the team title
                 var teamTitle = team.text1.replace('team', '').trim();
-                html += '<p>' + teamTitle + '</p>';
+                html += '<p><a href="' + teamTitle + '" target="_blank"><i>Website</i></a></p>';
             }
-            if ('text2' in team && Array.isArray(team.text2)) {
-                html += '<ul>'; // Start the unordered list
-                for (var j = 0; j < team.text2.length; j++) {
-                    html += '<li>' + team.text2[j] + '</li>'; // Add each list item
-                }
-                html += '</ul>'; // Close the unordered list
+            if ('text2' in team) {
+                // Remove the word "team" from the team title
+                var teamTitle = team.text2.replace('team', '').trim();
+                html += '<p>' + teamTitle + '</p>';
             }     
         }
     }
