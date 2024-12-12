d97637b5476c5699c628fc683b8fc06af6a31792
pranayajajoo <pranayajajoo@gmail.com>
2024-12-11 18:10:22 -0700

    small layout changes

--- page.js
+++ page.js
@@ -10,7 +10,7 @@ var data = {
                         // teamURLS: ['https://hari-sikchi.github.io/', 'https://agarwalsiddhant10.github.io/', 'https://pranayajajoo.github.io/', 'https://samp830.github.io/', 'http://calcharles.github.io/', 'https://maxrudolph1.github.io/', 'https://www.cs.utexas.edu/~pstone/', 'https://www.ece.utexas.edu/people/faculty/amy-zhang', 'https://people.cs.umass.edu/~sniekum/'],
                         period: 'arxiv.org/abs/2412.05718',
                         text1: 'https://hari-sikchi.github.io/rlzero/',
-                        text2: 'Releasing soon!',
+                        text2: '',
                     }
                    ]
         },
